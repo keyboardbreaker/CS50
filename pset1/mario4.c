2dec98b23f983014f74892e6f3fc5cd579310aed
Yu-Jin Tan <hello@yjt.yt>
2018-05-29 12:05:37 +0000

    making a square of n x n in  mario4.c

---
+++ pset1/mario4.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    // Prompt user for a positive number
+    int n;
+
+    do{
+        n = get_int("Positive number: ");
+    }
+    while(n <= 0);
+
+    //print n many rows
+    for(int i = 0; i < n; i++)
+    {
+        //print n many columns
+        for(int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
